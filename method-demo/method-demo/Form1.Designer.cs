f20820f31329f7bf77867db891388a6874651228
Anil Mandal <anil_mndl@yahoo.com>
2017-10-05 09:26:54 -0600

    initial commit

---
+++ method-demo/method-demo/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace method_demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblHello = new System.Windows.Forms.Label();
+            this.LblWorld = new System.Windows.Forms.Label();
+            this.textBoxTemp1 = new System.Windows.Forms.TextBox();
+            this.textBoxTemp2 = new System.Windows.Forms.TextBox();
+            this.BtnGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LblHello
+            // 
+            this.LblHello.AutoSize = true;
+            this.LblHello.Location = new System.Drawing.Point(71, 45);
+            this.LblHello.Name = "LblHello";
+            this.LblHello.Size = new System.Drawing.Size(19, 13);
+            this.LblHello.TabIndex = 0;
+            this.LblHello.Text = "na";
+            // 
+            // LblWorld
+            // 
+            this.LblWorld.AutoSize = true;
+            this.LblWorld.Location = new System.Drawing.Point(71, 103);
+            this.LblWorld.Name = "LblWorld";
+            this.LblWorld.Size = new System.Drawing.Size(19, 13);
+            this.LblWorld.TabIndex = 1;
+            this.LblWorld.Text = "na";
+            // 
+            // textBoxTemp1
+            // 
+            this.textBoxTemp1.Location = new System.Drawing.Point(158, 38);
+            this.textBoxTemp1.Name = "textBoxTemp1";
+            this.textBoxTemp1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTemp1.TabIndex = 2;
+            // 
+            // textBoxTemp2
+            // 
+            this.textBoxTemp2.Location = new System.Drawing.Point(158, 103);
+            this.textBoxTemp2.Name = "textBoxTemp2";
+            this.textBoxTemp2.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTemp2.TabIndex = 3;
+            // 
+            // BtnGo
+            // 
+            this.BtnGo.Location = new System.Drawing.Point(74, 197);
+            this.BtnGo.Name = "BtnGo";
+            this.BtnGo.Size = new System.Drawing.Size(116, 23);
+            this.BtnGo.TabIndex = 4;
+            this.BtnGo.Text = "Go Get Them";
+            this.BtnGo.UseVisualStyleBackColor = true;
+            this.BtnGo.Click += new System.EventHandler(this.BtnGo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.BtnGo);
+            this.Controls.Add(this.textBoxTemp2);
+            this.Controls.Add(this.textBoxTemp1);
+            this.Controls.Add(this.LblWorld);
+            this.Controls.Add(this.LblHello);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblHello;
+        private System.Windows.Forms.Label LblWorld;
+        private System.Windows.Forms.TextBox textBoxTemp1;
+        private System.Windows.Forms.TextBox textBoxTemp2;
+        private System.Windows.Forms.Button BtnGo;
+    }
+}
+
---
+++ method-demo/method-demo/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace method_demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGo_Click(object sender, EventArgs e)
+        {
+            SayHello();
+
+            bool IsIcyHot = IcyHot(-1, 120);
+            MessageBox.Show(IsIcyHot.ToString());
+
+            bool checkIcyHot = IcyHot();
+            MessageBox.Show(checkIcyHot.ToString());
+
+            int answer = IntMax(4, 4, 23);
+            MessageBox.Show(answer.ToString());
+
+            bool AreWeInTrouble = MonkeyTrouble(false, false);
+            MessageBox.Show(AreWeInTrouble.ToString());
+
+            double diff = Diff21(19);
+            MessageBox.Show(diff.ToString());
+        }
+
+
+        /// <summary>Set text property of labels to Hello and World</summary>
+        /// <returns>void</returns>
+        private void SayHello()
+        {
+            LblHello.Text = "Hello";
+            LblWorld.Text = "World";
+        }
+
+
+        /// <summary>Check if it is icy hot</summary>
+        /// <param name="temp1">Temperature 1</param>
+        /// <param name="temp2">Temperature 2</param>
+        /// <returns>bool</returns>
+        private bool IcyHot(int temp1, int temp2)
+        {
+            if ((temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0))       // error here
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool IcyHot()
+        {
+            int temp1, temp2;
+
+            int.TryParse(textBoxTemp1.Text, out temp1);
+            int.TryParse(textBoxTemp2.Text, out temp2);
+
+            if (temp1 < 0 && temp2 > 100)       // error here
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private int IntMax(int a, int b, int c)
+        {
+            if (a > b && a > c)
+            {
+                return a;
+
+            }
+            else if (b > a && b > c)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
+        }
+
+        private bool MonkeyTrouble(bool aSmile, bool bSmile)
+        {
+            if (aSmile == true ^ bSmile == true)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private double Diff21(int n)
+        {
+            if (n > 21)
+            {
+                return Math.Abs(2 * (n - 21));
+            }
+            else
+            {
+                return Math.Abs(n - 21);
+            }
+        }
+    }
+}
